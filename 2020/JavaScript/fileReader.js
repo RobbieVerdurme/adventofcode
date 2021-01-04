46722acadd2002484d8ea63927c46f3d6ddbee21
RobbieVerdurme <robbievrdrm@gmail.com>
2021-01-04 21:39:42 +0100

    update filereader so no empty values can be returned

--- 2020/JavaScript/fileReader.js
+++ 2020/JavaScript/fileReader.js
@@ -1,7 +1,10 @@
+const { strict } = require('assert');
+
 function readFile(filePath){
     var fs = require('fs');
     var text = fs.readFileSync(filePath).toString();
-    return text.split('\n');
+    
+    return text.split('\n').map(x => /[^\s]*/g.exec(x)[0])
 }
 
 module.exports = {readFile}
